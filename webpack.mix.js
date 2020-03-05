c9d91e28f459f5e231efd5edfaf4edf299359021
Haja <haja.joelinjatovo@emediaplace.com>
2020-03-05 16:57:06 +0300

    Assets compiling

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,21 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/admin.scss', 'public/css/admin/styles.css')
+   .sass('resources/sass/driver.scss', 'public/css/driver/styles.css')
+   .sass('resources/sass/customer.scss', 'public/css/customer/styles.css');
+
+/**
+mix.styles([
+    'public/css/vendor/normalize.css',
+    'public/css/vendor/videojs.css'
+], 'public/css/all.css');
+
+mix.js('resources/js/app.js', 'public/js')
+    .extract(['vue'])
+    => public/js/manifest.js: The Webpack manifest runtime
+    => public/js/vendor.js: Your vendor libraries
+    => public/js/app.js: Your application code
+    
+*/
