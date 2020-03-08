9911b99f85d11ada5f798bb86540bbecd9d0b7a4
Joelinjatovo <joelinjatovo@gmail.com>
2020-03-08 17:45:18 +0300

    Database seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -38,6 +38,7 @@ class UserSeeder extends Seeder
                 'user_id' => 1,
                 'created_at' => now(),
             ]);
+            $++;
         }
         
         DB::table('role_user')->insert(['approved' => 1, 'user_id' => 1, 'role_id' => 1]);
