f2adfd9282233bc0c69631f85696c1a0faa477d0
Joelinjatovo <joelinjatovo@gmail.com>
2020-03-23 21:35:31 +0300

    Home

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -53,7 +53,7 @@ class Car extends Model
      */
     public function model()
     {
-        return $this->belongsTo(CarModel::class);
+        return $this->belongsTo(CarModel::class, 'car_model_id');
     }
     
     /**
