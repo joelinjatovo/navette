776d3f8cf8d0429f42da9dfdafeaad4ac805637a
Joelinjatovo <joelinjatovo@gmail.com>
2020-05-19 23:51:42 +0300

    Admin car edit

--- resources/views/admin/car/edit.blade.php
+++ resources/views/admin/car/edit.blade.php
@@ -1,3 +1,42 @@
 @extends('admin.car.create')
 
-@section('content.title'){{ __('messages.form.car.edit') }}@endsection
+@section('title'){{ __('messages.car.edit') }}@endsection
+
+@section('subheader')
+<div class="subheader py-2 py-lg-4  subheader-solid " id="kt_subheader">
+    <div class=" container-fluid  d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+        <!--begin::Details-->
+        <div class="d-flex align-items-center flex-wrap mr-2">
+			
+            <!--begin::Title-->
+            <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">{{ __('messages.form.car.edit') }}</h5>
+            <!--end::Title-->
+
+            <!--begin::Separator-->
+            <div class="subheader-separator subheader-separator-ver mt-2 mb-2 mr-5 bg-gray-200"></div>
+            <!--end::Separator-->
+
+            <!--begin::User Name-->
+            <div class="d-flex align-items-center" id="kt_subheader_search">
+                <span class="text-dark-50 font-weight-bold" id="kt_subheader_total">{{ $model->name }}</span>
+            </div>
+            <!--end::User Name-->
+
+        </div>
+        <!--end::Details-->
+
+        <!--begin::Toolbar-->
+        <div class="d-flex align-items-center">
+            <!--begin::Button-->
+            <a href="{{ route('admin.cars') }}" class="btn btn-default font-weight-bold btn-sm px-3 font-size-base mr-2"><i class="la la-arrow-left"></i> {{ __('messages.button.back') }}</a>
+            <!--end::Button-->
+            
+            <!--begin::Button-->
+            <button type="submit" form="kt_form" class="btn btn-primary font-weight-bold btn-sm px-3 font-size-base"><i class="la la-check"></i>{{ __('messages.button.update') }}</button>
+            <!--end::Button-->
+                            
+        </div>
+        <!--end::Toolbar-->
+    </div>
+</div>
+@endsection
