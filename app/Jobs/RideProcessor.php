220f5df5e7aac15c1049007e4fa06b0743619fd7
Joelinjatovo <joelinjatovo@gmail.com>
2020-05-27 15:34:08 +0300

    Ride processor

--- app/Jobs/RideProcessor.php
+++ app/Jobs/RideProcessor.php
@@ -64,7 +64,111 @@ class RideProcessor implements ShouldQueue
      */
     protected function performTask(Item $item)
     {
-        // Attach this item to the ride
+		if($item->order && $item->order->car){
+			// Attach this item to the ride
+			$ride = $this->getPingedRide($item);
+			$start_date = now()->addMinutes(5);
+			if($ride){
+				$count_back = $ride->items()->where('items.type', Item::TYPE_BACK)->count();
+				$count_go = $ride->items()->where('items.type', Item::TYPE_GO)->count();
+				$count = $count_go + $count_back;
+				// Duree du trajet + Arret sur tous les point de ramassage + Arret sur le point de depart
+				$duration = $ride->duration + ( $count_go + 1 ) * 5 * 60;
+				$max = 60 * 60;
+
+				// TODO Check duration
+				$event_ride = new RideStatusChanged($ride, 'updated', null, $ride->status);
+			}else{
+				$active_ride = $this->getActivedRide($item);
+				if($active_ride){
+					// Duree du trajet + Arret sur tous les point de ramassage + Arret sur le point de depart
+					$count_go = $ride->items()->where('items.type', Item::TYPE_GO)->count();
+					$duration = $active_ride->duration + ( $count_go + 1 ) * 5 * 60;
+					$start_date = $active_ride->started_at->addSeconds($duration);
+				}
+
+				// Created new ride
+				$ride = new Ride();
+				$ride->status = Ride::STATUS_PING;
+				$ride->start_at = $start_date->addMinutes(5);
+				$ride->car()->associate($item->order->car);
+				$ride->driver()->associate($item->order->car->driver);
+				$ride->save();
+				
+				$event_ride = new RideStatusChanged($ride, 'created', null, $ride->status);
+			}
+			
+			// Attach the item's order point to the ride
+			$item_start_at = $ride->start_at->addMinutes(5)->addSeconds($item->duration_value);
+			$ride->points()->attach($item->point->getKey(), [
+				'status' => RidePoint::STATUS_PING,
+				'type' => ($item->type == Item::TYPE_BACK ? RidePoint::TYPE_DROP : RidePoint::TYPE_PICKUP),
+				'order' => 0,
+				'arrive_at' => $item_start_at,
+				'item_id' => $item->getKey(),
+				'user_id' => $item->user ? $item->user->getKey() : null,
+			]);
+
+
+			// Set item status ACTIVE
+			$oldStatus = $item->status;
+			$newStatus = Item::STATUS_ACTIVE;
+			$item->status = $newStatus;
+			$item->start_at = $item_start_at;
+			$item->ride()->associate($ride);
+			$item->driver()->associate($driver);
+			$item->save();
+			$event_item = new ItemStatusChanged($item, 'updated', $oldStatus, $newStatus);
+			
+			// Set order status ACTIVE
+			$oldStatus = $order->status;
+			$newStatus = Order::STATUS_ACTIVE;
+			$order->status = $newStatus;
+			$order->save();
+			$event_order = new OrderStatusChanged($order, 'updated', $oldStatus, $newStatus);
+
+			// Triger events
+			event($event_ride);
+			event($event_item);
+			event($event_order);
+		}
+    }
+
+    /**
+     * Get pinged ride
+     *
+     * @Param App\Models\Item $item
+     * @return mixed
+     */
+    protected function getPingedRide($item)
+    {
+		if($item->order && $item->order->car){
+        	$car = $item->order->car;
+			$query = Ride::where('car_id', $car->getKey())->where('status', Ride::STATUS_PING)->orderBy('start_at', 'asc');
+			if($item->ride_at){
+				$query->where('start_at', '>=', $item->ride_at);
+			}
+			return $query->first();
+		}
+		return null;
+    }
+
+    /**
+     * Get actived ride
+     *
+     * @Param App\Models\Item $item
+     * @return mixed
+     */
+    protected function getActivedRide($item)
+    {
+		if($item->order && $item->order->car){
+        	$car = $item->order->car;
+			$query = Ride::where('car_id', $car->getKey())->where('status', Ride::STATUS_ACTIVE)
+				->where('duration', '<', 15 * 60);
+				->orderBy('start_at', 'asc');
+			return $query->first();
+		}
+		return null;
     }
 
     /**
