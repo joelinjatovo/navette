19cb3e4682e44a54108c960ba41d4de6c3d07ed0
Joelinjatovo <joelinjatovo@gmail.com>
2020-05-28 01:31:31 +0300

    Ride point error

--- database/migrations/2020_04_16_010000_create_ride_point_table.php
+++ database/migrations/2020_04_16_010000_create_ride_point_table.php
@@ -36,7 +36,7 @@ class CreateRidePointTable extends Migration
                 $table->unsignedBigInteger('item_id')->nullable();
                 $table->foreign('item_id')->references('id')->on('items');
                 $table->unsignedBigInteger('user_id')->nullable();
-                $table->foreign('user_id')->references('id')->on('points');
+                $table->foreign('user_id')->references('id')->on('users');
                 $table->timestamps();
             });
         }
