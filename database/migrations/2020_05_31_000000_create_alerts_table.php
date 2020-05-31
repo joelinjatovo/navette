d3c6f667cb3d8bb83894b4a6eeddcea4cad66ec6
Joelinjatovo <joelinjatovo@gmail.com>
2020-06-01 02:17:17 +0300

    Alert

---
+++ database/migrations/2020_05_31_000000_create_alerts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if( ! Schema::hasTable('alerts') ) {
+			Schema::create('alerts', function (Blueprint $table) {
+				$table->id();
+                $table->boolean('is_public')->default(true);
+                $table->string('event');
+                $table->string('title');
+                $table->string('type');
+                $table->text('description');
+                $table->string('uri');
+				$table->timestamps();
+			});
+		}
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('alerts');
+    }
+}
