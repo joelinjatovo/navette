a149e734c7e0416e3bf1652970f8e36e2482b763
Joelinjatovo <joelinjatovo@gmail.com>
2020-06-01 03:04:04 +0300

    Item notification

--- app/Listeners/ItemEventSubscriber.php
+++ app/Listeners/ItemEventSubscriber.php
@@ -2,8 +2,28 @@
 
 namespace App\Listeners;
 
-use App\Events\ItemStatusChanged as ItemStatusChangedEvent;
-use App\Notifications\ItemStatus as ItemStatusNotification;
+use App\Events\Item\ItemActived as ItemActivedEvent;
+use App\Events\Item\ItemCanceled as ItemCanceledEvent;
+use App\Events\Item\ItemCompleted as ItemCompletedEvent;
+use App\Events\Item\ItemDateDelayed as ItemDateDelayedEvent;
+use App\Events\Item\ItemDateForwarded as ItemDateForwardedEvent;
+use App\Events\Item\ItemDateInited as ItemDateInitedEvent;
+use App\Events\Item\ItemDeleted as ItemDeletedEvent;
+use App\Events\Item\ItemDriverArrived as ItemDriverArrivedEvent;
+use App\Events\Item\ItemNexted as ItemNextedEvent;
+use App\Events\Item\ItemStarted as ItemStartedEvent;
+
+use App\Notifications\ItemActived as ItemActivedNotification;
+use App\Notifications\ItemCanceled as ItemCanceledNotification;
+use App\Notifications\ItemCompleted as ItemCompletedNotification;
+use App\Notifications\ItemDateDelayed as ItemDateDelayedNotification;
+use App\Notifications\ItemDateForwarded as ItemDateForwardedNotification;
+use App\Notifications\ItemDateInited as ItemDateInitedNotification;
+use App\Notifications\ItemDeleted as ItemDeletedNotification;
+use App\Notifications\ItemDriverArrived as ItemDriverArrivedNotification;
+use App\Notifications\ItemNexted as ItemNextedNotification;
+use App\Notifications\ItemStarted as ItemStartedNotification;
+
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -16,18 +36,48 @@ class ItemEventSubscriber
      */
     public function subscribe($events)
     {
-        $events->listen(
-            'App\Events\ItemStatusChanged',
-            'App\Listeners\ItemEventSubscriber@handle'
-        );
+        $events->listen(ItemActivedEvent::class, self::class .'@handle');
     }
     
     /**
-     * Hande Ride status changed events.
+     * Hande events.
      */
-    public function handle(ItemStatusChangedEvent $event) {
-        if($event->item && $event->item->order && $event->item->order->user) {
-            $event->item->order->user->notify(new ItemStatusNotification($event->item, $event->oldStatus, $event->newStatus));
-        }
+    public function handle($event) {
+		if( !($item = $event->item) || !($user = $item->user) ){
+			return;
+		}
+
+		switch(true){
+			case $event instanceof ItemActivedEvent:
+				$user->notify(new ItemActivedNotification($item));
+			break;
+			case $event instanceof ItemCanceledEvent:
+				$user->notify(new ItemCanceledNotification($item));
+			break;
+			case $event instanceof ItemCompletedEvent:
+				$user->notify(new ItemCompletedNotification($item));
+			break;
+			case $event instanceof ItemDateDelayedEvent:
+				$user->notify(new ItemDateDelayedNotification($item));
+			break;
+			case $event instanceof ItemDateForwardedEvent:
+				$user->notify(new ItemDateForwardedNotification($item));
+			break;
+			case $event instanceof ItemDateInitedEvent:
+				$user->notify(new ItemDateInitedNotification($item));
+			break;
+			case $event instanceof ItemDeletedEvent:
+				$user->notify(new ItemDeletedNotification($item));
+			break;
+			case $event instanceof ItemDriverArrivedEvent:
+				$user->notify(new ItemDriverArrivedNotification($item));
+			break;
+			case $event instanceof ItemNextedEvent:
+				$user->notify(new ItemNextedNotification($item));
+			break;
+			case $event instanceof ItemStartedEvent:
+				$user->notify(new ItemStartedNotification($item));
+			break;
+		}
     }
 }
