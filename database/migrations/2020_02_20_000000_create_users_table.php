ff4070c8e70f787707edd002bfdf7bd5fd28969d
Joelinjatovo <joelinjatovo@gmail.com>
2020-06-10 23:11:26 +0300

    Code de parainnage

--- database/migrations/2020_02_20_000000_create_users_table.php
+++ database/migrations/2020_02_20_000000_create_users_table.php
@@ -19,10 +19,10 @@ class CreateUsersTable extends Migration
                 $table->string('stripe_id', 200)->nullable();
                 $table->string('facebook_id', 200)->nullable();
                 $table->uuid('code')->nullable()->unique();
-                $table->string('first_name', 100);
-                $table->string('last_name', 100);
-                $table->string('birthday', 100);
-                $table->string('name', 100);
+                $table->string('first_name', 100)->nullable();
+                $table->string('last_name', 100)->nullable();
+                $table->string('birthday', 100)->nullable();
+                $table->string('name', 100)->nullable();
                 $table->string('email', 100)->nullable();
                 $table->string('phone', 20)->nullable();
                 $table->string('password');
